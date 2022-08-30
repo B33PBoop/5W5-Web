2f8798de25ca16d373f3f9e8385667f6a85de5fa
Jean-Romain <77986513+B33PBoop@users.noreply.github.com>
2022-08-30 10:20:32 -0400

    création des fichiers css

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<!-- insert footer content here -->
+<!-- insert footer content here ( socials, credits, maybe a menu for the site-->
 
 </body>
 <?php wp_footer() ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php get_header() ?>
     
 <!-- insert main here -->
+
+
+<?php get_footer() ?>
