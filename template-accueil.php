91b5d3e3d9c24f1908f2f43a3e5e5c28ef3a9da4
Alex <Yukako22@live.fr>
2022-10-29 19:01:12 -0400

    accueil

--- template-accueil.php
+++ template-accueil.php
@@ -24,10 +24,12 @@ while ( have_posts() ) : the_post();
     <?php
     if ( $gallery = get_post_gallery( get_the_ID(), false ) ) :
         // Loop through all the image and output them one by one.
+        
+        echo "<div class='container_carousel'>";
         echo "<button class='bouton_droite bouton_carousel' onclick='carousel(`droite`)'><span class='material-symbols-outlined'>
         keyboard_double_arrow_left
         </span></button>";
-        echo "<div class='container_carousel'>";
+        
         $galleryIds = explode( ",", $gallery['ids'] );
 
         foreach ( $galleryIds as $id) {
@@ -42,10 +44,12 @@ while ( have_posts() ) : the_post();
             <?php
             
         }
-        echo "</div>";
         echo "<button class='bouton_gauche bouton_carousel' onclick='carousel(`gauche`)'><span class='material-symbols-outlined'>
         keyboard_double_arrow_right
         </span></button>";
+        echo "</div>";
+        
+        
     endif;
 endwhile;
 ?>
