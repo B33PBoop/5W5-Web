d9f60523bd0aa199074cb0d704725fd6a991f714
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-10-29 15:10:07 -0400

    completion de la base de la page principale

--- template-accueil.php
+++ template-accueil.php
@@ -11,9 +11,20 @@
 <?php
 /* The loop */
 while ( have_posts() ) : the_post();
+
+    the_title("<h2 class='title'>", "</h2>");
+
+    $text = apply_filters('the_content', get_the_content() );
+    $paragraphs = explode('</p>', $text);
+    ?>
+    <div class="container_description">
+    <div class="description"> <?php echo $paragraphs[0]?></div>
+    <button class="savoir_plus"> <?php echo $paragraphs[1]?> </button>
+    </div>
+    <?php
     if ( $gallery = get_post_gallery( get_the_ID(), false ) ) :
         // Loop through all the image and output them one by one.
-        echo "<button class='bouton_droite' onclick='carousel(`droite`)'><span class='material-symbols-outlined'>
+        echo "<button class='bouton_droite bouton_carousel' onclick='carousel(`droite`)'><span class='material-symbols-outlined'>
         keyboard_double_arrow_left
         </span></button>";
         echo "<div class='container_carousel'>";
@@ -32,7 +43,7 @@ while ( have_posts() ) : the_post();
             
         }
         echo "</div>";
-        echo "<button class='bouton_gauche' onclick='carousel(`gauche`)'><span class='material-symbols-outlined'>
+        echo "<button class='bouton_gauche bouton_carousel' onclick='carousel(`gauche`)'><span class='material-symbols-outlined'>
         keyboard_double_arrow_right
         </span></button>";
     endif;
